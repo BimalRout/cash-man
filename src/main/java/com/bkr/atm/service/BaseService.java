bcf2deeee23753d01dcb1485999f59636c0db899
Bimal <bkr.nist@gmail.com>
2018-04-02 23:02:12 +0530

    commit

--- src/main/java/com/bkr/atm/service/BaseService.java
+++ src/main/java/com/bkr/atm/service/BaseService.java
@@ -1,7 +1,5 @@
 package com.bkr.atm.service;
 
-import org.springframework.transaction.annotation.Isolation;
-import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 @Transactional
